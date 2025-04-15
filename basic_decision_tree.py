ba59d35366bb0a6b14cd33094f0497fc22cb6231
elijahmccauley <emmccauley6@gmail.com>
2025-04-14 23:03:17 -0400

    Add gridcv to fix hyperparameter tuning

--- basic_decision_tree.py
+++ basic_decision_tree.py
@@ -7,6 +7,7 @@ import pandas as pd
 import os
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import plot_tree
+from sklearn.model_selection import GridSearchCV
 
 """stock_folder = "./clean_data"
 stock_files = [f for f in os.listdir(stock_folder) if f.endswith('.csv')]
@@ -38,13 +39,40 @@ stock_data = stock_data.drop('Ticker', axis=1)
 X = stock_data.drop('Target', axis=1)
 y = stock_data['Target']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-dt_classifier = DecisionTreeClassifier()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+"""param_grid = {
+    'max_depth': [3, 5, 10, 15, 20, None],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': ['sqrt', 'log2', None]
+}
+
+clf = DecisionTreeClassifier(random_state=42)
+grid_search = GridSearchCV(clf, param_grid, cv=5, scoring='accuracy')
+grid_search.fit(X_train, y_train)
+
+print("Best Parameters:", grid_search.best_params_)"""
+# Best Parameters: {'max_depth': 20, 'max_features': None, 'min_samples_leaf': 4, 'min_samples_split': 10}
+
+
+dt_classifier = DecisionTreeClassifier(max_depth=20, max_features=None, min_samples_leaf=4, min_samples_split=10)
 dt_classifier.fit(X_train, y_train)
 
 y_pred = dt_classifier.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
+test_accuracy = accuracy_score(y_test, y_pred)
+print(f"Test Accuracy: {test_accuracy}")
 
 plot_tree(dt_classifier)
 
+train_y_pred = dt_classifier.predict(X_train)
+train_accuracy = accuracy_score(y_train, train_y_pred)
+print(f"Train Accuracy: {train_accuracy}")
+
+# no hyperparameters
+#Test Accuracy: 0.5922388725885959
+#Train Accuracy: 1.0
+
+# best hyper parameters
+#Test Accuracy: 0.6124162285279194
+#Train Accuracy: 0.7703540336114229
