75683bc1b61f8a5c4dd0ee62413efde922a6305b
elijahmccauley <emmccauley6@gmail.com>
2025-04-21 01:35:52 -0400

    Update linear regression predictions

--- linear_regression.py
+++ linear_regression.py
@@ -7,20 +7,6 @@ import os
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import GridSearchCV
 
-"""stock_folder = "./clean_data"
-stock_files = [f for f in os.listdir(stock_folder) if f.endswith('.csv')]
-print(stock_files)
-dataframes = []
-
-for file in stock_files:
-    file_path = os.path.join(stock_folder, file)
-    print(file, file_path)
-    df = pd.read_csv(file_path)
-    df['Ticker'] = file.split('.')[0]
-    dataframes.append(df)
-
-merged_df = pd.concat(dataframes, ignore_index=True)
-merged_df.to_csv('merged_data.csv', index=False)"""
 
 stock_data = pd.read_csv('./merged_data.csv')
 stock_data['Pct_Change'] = (stock_data['Close'].shift(-1) - stock_data['Close']) / stock_data['Close']
@@ -40,20 +26,6 @@ y = stock_data['Pct_Change']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
-"""param_grid = {
-    'max_depth': [3, 5, 10, 15, 20, None],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4],
-    'max_features': ['sqrt', 'log2', None]
-}
-
-clf = DecisionTreeClassifier(random_state=42)
-grid_search = GridSearchCV(clf, param_grid, cv=5, scoring='accuracy')
-grid_search.fit(X_train, y_train)
-
-print("Best Parameters:", grid_search.best_params_)"""
-# Best Parameters: {'max_depth': 20, 'max_features': None, 'min_samples_leaf': 4, 'min_samples_split': 10}
-
 
 reg = LinearRegression()
 reg.fit(X_train, y_train)
@@ -66,9 +38,20 @@ threshold = 0.005  # 0.5%
 
 profitable_predictions = (y_pred > threshold)
 num_trades = profitable_predictions.sum()
+returns = y_test[profitable_predictions]
+quants = np.quantile(returns, [0, 0.05, 0.5, .95, 1])
+normalized_returns = []
+for i in returns:
+    if i > quants[1] and i < quants[3]:
+        normalized_returns.append(i)
+
 avg_actual_return = y_test[profitable_predictions].mean()
+avg_selected_returns = sum(normalized_returns) / len(normalized_returns)
 
-print(f"Number of trades: {num_trades}")
-print(f"Avg return on selected trades: {avg_actual_return:.4f}")
-
+print(f"Number of selected trades: {num_trades}")
+print(f"Number of middle 90% trades: {len(normalized_returns)}")
+print(f"Avg return on all selected trades: {avg_actual_return:.4f}")
+print(f"Avg return on filtered selected trades: {avg_selected_returns:.4f}")
+print(y_test[profitable_predictions].max())
 # base model avg returns on selected trades = 1.32%
+# returns of middle 90% = 0.0113
